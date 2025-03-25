4b7ece08f5bcd18f7d125cb0dc248823a4f39177
JoseCarlos2907 <josecarlosperezgonzalez200@gmail.com>
2025-03-25 13:41:50 +0100

    inicio de sesion con correo añadido

--- src/main/java/com/iesfernandoaguilar/perezgonzalez/controller/Lector_InicioSesion.java
+++ src/main/java/com/iesfernandoaguilar/perezgonzalez/controller/Lector_InicioSesion.java
@@ -44,11 +44,9 @@ public class Lector_InicioSesion extends Thread{
                     // System.out.println("INICIA_SESION");
                     if("si".equals(msgServidor.getParams().get(0))){
                         iniciaSesion = true;
-                        System.out.println("Inicia sesion");
                         usuarioJSON = msgServidor.getParams().get(1);
                     }else if("no".equals(msgServidor.getParams().get(0))){
                         this.controllerInicioSesion.inicioDeSesionIncorrecto();
-                        System.out.println("Incorrecto");
                     }
                 }
             } catch (EOFException e) {
