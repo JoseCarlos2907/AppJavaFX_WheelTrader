6904b114b9b82c4f134235a16ec1230e9724d418
JoseCarlos2907 <josecarlosperezgonzalez200@gmail.com>
2025-06-10 19:28:32 +0200

    limpieza de lo que no es codigo tal cual

--- build/wheeltrader/usr/bin/wheeltrader.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Dprism.order=sw -Dprism.forceGPU=true -Dsun.java2d.xrender=true --module-path /usr/lib/wheeltrader/javafx --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar /usr/lib/wheeltrader/wheeltrader-1.0-jar-with-dependencies.jar
